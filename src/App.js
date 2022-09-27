83947b332b2466377ac991b6286813c7050a5ab6
Kelly-kells <kellskelechi@gmail.com>
2022-09-27 16:23:58 +0100

    folder-structure

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
 import React from 'react'
+import Header from './Compnents/header/Header'
+import Navbar from './Compnents/navbar/Navbar'
+import About from './Compnents/about/About'
+import Experience from './Compnents/experience/Experience'
+import Services from './Compnents/services/Services'
+import Portfolio from './Compnents/portfolio/Portfolio'
+import Contact from './Compnents/portfolio/Portfolio'
+import Folder from './Compnents/footer/Footer'
 
 const App = () => {
   return (
-    <div>App</div>
+    <>
+    < Header/>
+    <Navbar/>
+    <About/>
+    <Experience />
+    <Services/>
+    <Portfolio/>
+    <Contact/>
+    <Folder/>
+  </>
+    
   )
 }
 
