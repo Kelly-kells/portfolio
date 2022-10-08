69b79f08fd12e8597a22f130a52249052ff22bba
Kelly-kells <kellskelechi@gmail.com>
2022-10-08 09:22:10 +0100

    header

--- src/Compnents/header/Header.jsx
+++ src/Compnents/header/Header.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import CTA from './CTA'
+import me from '../../Assets/me-img.JPG'
+import style from './header.css'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <header>
+<div className="container header__container">
+  <h5>
+    Hello! I'm 
+  </h5>
+  <h1>Kelechi Favour</h1>
+  <h5 className="text-light">A frontend developer</h5>
+  <CTA/>
+  <div className="me">
+    <img src={me}alt="me" />
+  </div>
+</div>
+    </header>
   )
 }
 
