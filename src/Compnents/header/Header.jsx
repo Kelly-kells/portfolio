229b51c7128dc686a9444a8f3a1550c170674513
Kelly-kells <kellskelechi@gmail.com>
2022-10-09 20:27:06 +0100

    header-styling

--- src/Compnents/header/Header.jsx
+++ src/Compnents/header/Header.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import CTA from './CTA'
 import me from '../../Assets/me-img.JPG'
 import style from './header.css'
+import HeaderSocials from './HeaderSocials'
 
 const Header = () => {
   return (
@@ -13,9 +14,12 @@ const Header = () => {
   <h1>Kelechi Favour</h1>
   <h5 className="text-light">A frontend developer</h5>
   <CTA/>
+  <HeaderSocials/>
   <div className="me">
     <img src={me}alt="me" />
   </div>
+  
+  <a href="#contact" className='scroll-down'>scroll down</a>
 </div>
     </header>
   )
