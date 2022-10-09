c7225412b82da3284f8a1d2aa6cac1ad15d68eb8
Kelly-kells <kellskelechi@gmail.com>
2022-10-09 21:05:31 +0100

    header updated

--- src/Compnents/header/Header.jsx
+++ src/Compnents/header/Header.jsx
@@ -7,20 +7,20 @@ import HeaderSocials from './HeaderSocials'
 const Header = () => {
   return (
     <header>
-<div className="container header__container">
-  <h5>
-    Hello! I'm 
-  </h5>
-  <h1>Kelechi Favour</h1>
-  <h5 className="text-light">A frontend developer</h5>
-  <CTA/>
-  <HeaderSocials/>
-  <div className="me">
-    <img src={me}alt="me" />
-  </div>
-  
-  <a href="#contact" className='scroll-down'>scroll down</a>
-</div>
+      <div className="container header__container">
+        <h5>
+          Hello! I'm
+        </h5>
+        <h1>Kelechi Favour</h1>
+        <h5 className="text-light">A frontend developer</h5>
+        <CTA />
+        <HeaderSocials />
+        <div className="me">
+          <img src={me} alt="me" />
+        </div>
+
+        <a href="#contact" className='scroll-down'>scroll down</a>
+      </div>
     </header>
   )
 }
--- src/Compnents/header/HeaderSocials.jsx
+++ src/Compnents/header/HeaderSocials.jsx
@@ -1,17 +1,17 @@
 import React from 'react'
-import {BsLinkedin} from "react-icons/bs"
-import {BsGithub} from "react-icons/bs"
-import {AiFillTwitterSquare} from "react-icons/ai"
+import { BsLinkedin } from "react-icons/bs"
+import { BsGithub } from "react-icons/bs"
+import { AiFillTwitterSquare } from "react-icons/ai"
 
 const HeaderSocials = () => {
-  return (
-    <div className='header__socials'>
-        <a href="https://linked.com" target="_blank"><BsLinkedin/></a>
-        <a href="https://github.com" target="_blank"> <BsGithub/></a>
-        <a href="https://twitter.com" target="_blank"><AiFillTwitterSquare/></a>
+    return (
+        <div className='header__socials'>
+            <a href="https://linked.com" target="_blank"><BsLinkedin /></a>
+            <a href="https://github.com" target="_blank"> <BsGithub /></a>
+            <a href="https://twitter.com" target="_blank"><AiFillTwitterSquare /></a>
 
-    </div>
-  )
+        </div>
+    )
 }
 
 export default HeaderSocials
