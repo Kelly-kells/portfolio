4a6ff7b733122767ac3295e3980c28523fff7bad
Kelly-kells <kellskelechi@gmail.com>
2022-10-19 20:56:46 +0100

    about-styling

--- src/Components/about/About.jsx
+++ src/Components/about/About.jsx
@@ -3,7 +3,8 @@ import "./about.css"
 import techgirl from "../../Assets/techgirl.png"
 import { FaAward } from "react-icons/fa"
 import { FiUsers } from "react-icons/fi"
-import {AiOutlineFolderOpen} from 'react-icons/ai'
+import { AiOutlineFolderOpen } from 'react-icons/ai'
+import styles from "./about.css"
 
 const About = () => {
   return (
@@ -14,7 +15,7 @@ const About = () => {
       <h2>Me</h2>
       <div className="container container__about">
         <div className="about__me">
-          <div className="about__me image">
+          <div className="about__me-image">
             <img src={techgirl} alt="" />
           </div>
 
@@ -48,6 +49,10 @@ const About = () => {
 
             </article>
           </div>
+          <p>Creative Front-End Developer offering 2 years of experience providing high-impact web solutions for diverse industry organizations. Skilled in designing, developing and testing multiple web-based applications incorporating a range of technologies. Aspiring to combine broad background with strong technical skills to excel as a Front-End Developer.
+
+          </p>
+          <a href="#contact" className='btn btn-primary'> Contact me</a>
         </div>
       </div>
     </section>
