0c5e4c8196fb9048d9be088751ff86060424f55b
Kelly-kells <kellskelechi@gmail.com>
2022-10-28 14:58:26 +0100

    style

--- src/Components/about/About.jsx
+++ src/Components/about/About.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import "./about.css"
-import techgirl from "../../Assets/techgirl.png"
 import { FaAward } from "react-icons/fa"
 import { FiUsers } from "react-icons/fi"
 import { AiOutlineFolderOpen } from 'react-icons/ai'
@@ -9,16 +8,12 @@ import styles from "./about.css"
 const About = () => {
   return (
     <section id='about'>
-      <h5>
+      <h2>
         Get to know
-      </h5>
+      </h2>
       <h2>Me</h2>
       <div className="container container__about">
         <div className="about__me">
-          <div className="about__me-image">
-            <img src={techgirl} alt="" />
-          </div>
-
         </div>
         <div className="about__content">
           <div className="about__cards">
@@ -48,11 +43,16 @@ const About = () => {
               </small>
 
             </article>
+
+
           </div>
+          <div>
           <p>Creative Front-End Developer offering 2 years of experience providing high-impact web solutions for diverse industry organizations. Skilled in designing, developing and testing multiple web-based applications incorporating a range of technologies. Aspiring to combine broad background with strong technical skills to excel as a Front-End Developer.
 
-          </p>
-          <a href="#contact" className='btn btn-primary'> Contact me</a>
+</p>
+<a href="#contact" className='btn btn-primary'> Contact me</a>
+          </div>
+          
         </div>
       </div>
     </section>
