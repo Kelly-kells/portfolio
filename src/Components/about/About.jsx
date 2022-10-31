2d621d88c3463cb1680a1a227dc1d922569a79ba
Kelly-kells <kellskelechi@gmail.com>
2022-10-31 21:22:59 +0100

    responsive

--- src/Components/about/About.jsx
+++ src/Components/about/About.jsx
@@ -9,16 +9,16 @@ const About = () => {
   return (
     <section id='about'>
       <h2>
-        Get to know
+        Get to know me
       </h2>
-      <h2>Me</h2>
+      
       <div className="container container__about">
         <div className="about__me">
         </div>
         <div className="about__content">
           <div className="about__cards">
             <article className='about__card'>
-              <FaAward />
+              <FaAward size="30px" color="#8ecae6"/>
               <h5>Experience</h5>
               <small>
                 1+ years experience experience
@@ -27,7 +27,7 @@ const About = () => {
             </article>
 
             <article className='about__card'>
-              <FiUsers />
+              <FiUsers size="30px" color="#8ecae6"/>
               <h5>Clients</h5>
               <small>
                 freelance/personal projects
@@ -36,21 +36,17 @@ const About = () => {
             </article>
 
             <article className='about__card'>
-              <AiOutlineFolderOpen />
+              <AiOutlineFolderOpen size="30px" color="#8ecae6" />
               <h5>Projects</h5>
               <small>
                 1+ years experience experience
               </small>
 
             </article>
-
-
           </div>
           <div>
-          <p>Creative Front-End Developer offering 2 years of experience providing high-impact web solutions for diverse industry organizations. Skilled in designing, developing and testing multiple web-based applications incorporating a range of technologies. Aspiring to combine broad background with strong technical skills to excel as a Front-End Developer.
-
-</p>
-<a href="#contact" className='btn btn-primary'> Contact me</a>
+          <p>Creative Front-End Developer offering 2 years of experience providing high-impact web solutions for diverse industry organizations. Skilled in designing, developing and testing multiple web-based applications incorporating a range of technologies. Aspiring to combine broad background with strong technical skills to excel as a Front-End Developer.</p>
+          <a href="#contact" className='btn btn-primary'> Contact me</a>
           </div>
           
         </div>
