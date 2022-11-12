57bdf3da7a3513453405e40bb057d4b4e55de965
Kelly-kells <kellskelechi@gmail.com>
2022-11-12 15:20:02 +0100

    experience-section-styling

--- src/Components/experience/Experience.jsx
+++ src/Components/experience/Experience.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import {AiFillCheckCircle} from "react-icons/ai"
+import stlyes from "./experience.css"
 
 const experience = () => {
   return (
@@ -16,45 +17,59 @@ const experience = () => {
             <div className="experience__content">
 
               <article className='details'>
-             <AiFillCheckCircle color="blue" size="25px"/>
+             <AiFillCheckCircle color="#8ecae6" size="25px"/>
+             <div>
              <h4>HTML</h4>
              <small className='text-light'>Experienced</small>
+             </div>
               </article>
 
               <article className='details'>
-             <AiFillCheckCircle color="blue" size="25px"/>
+             <AiFillCheckCircle color="#8ecae6" size="25px"/>
+             <div>
              <h4>CSS</h4>
              <small className='text-light'>Intermediate</small>
+             </div>
               </article>
 
               <article className='details'>
-             <AiFillCheckCircle color="blue" size="25px"/>
-             <h4>Javascript</h4>
-             <small className='text-light'>intermediate</small>
+             <AiFillCheckCircle color="#8ecae6" size="25px"/>
+             <div>
+             <h4>CSS</h4>
+             <small className='text-light'>Intermediate</small>
+             </div>
               </article>
 
               <article className='details'>
-             <AiFillCheckCircle color="blue" size="25px"/>
+             <AiFillCheckCircle color="#8ecae6" size="25px"/>
+             <div>
              <h4>React.Js</h4>
              <small className='text-light'>Experienced</small>
+             </div>
               </article>
 
               <article className='details'>
-             <AiFillCheckCircle color="blue" size="25px"/>
-             <h4>Redux</h4>
+             <AiFillCheckCircle color="#8ecae6" size="25px"/>
+            <div>
+            <h4>Redux</h4>
              <small className='text-light'>Experienced</small>
+            </div>
               </article>
 
               <article className='details'>
-             <AiFillCheckCircle color="blue" size="25px"/>
+             <AiFillCheckCircle color="#8ecae6" size="25px"/>
+             <div>
              <h4>Boostrap</h4>
              <small className='text-light'>Experienced</small>
+             </div>
               </article>
 
               <article className='details'>
-             <AiFillCheckCircle color="blue" size="25px"/>
-             <h4>Tailwind</h4>
+             <AiFillCheckCircle color="#8ecae6" size="25px"/>
+             <div>
+             <h4>Boostrap</h4>
              <small className='text-light'>Experienced</small>
+             </div>
               </article>
 
               
@@ -62,49 +77,36 @@ const experience = () => {
             </div>
           </div>
           <div className="experience__backend">
+   <h3>Backend Development</h3>
+   <div className="experience__content">
 
-          <article className='details'>
-             <AiFillCheckCircle color="blue" size="25px"/>
-             <h4>HTML</h4>
-             <small className='text-light'>Experienced</small>
+   <article className='details'>
+             <AiFillCheckCircle color="#8ecae6" size="25px"/>
+             <div>
+             <h4>Node.js</h4>
+             <small className='text-light'>Beginner</small>
+             </div>
               </article>
 
               <article className='details'>
-             <AiFillCheckCircle color="blue" size="25px"/>
-             <h4>CSS</h4>
-             <small className='text-light'>Intermediate</small>
+             <AiFillCheckCircle color="#8ecae6" size="25px"/>
+             <div><h4>PHP</h4>
+             <small className='text-light'>Beginner</small>
+             </div>
               </article>
 
               <article className='details'>
-             <AiFillCheckCircle color="blue" size="25px"/>
-             <h4>Javascript</h4>
-             <small className='text-light'>intermediate</small>
+             <AiFillCheckCircle color="#8ecae6" size="25px"/>
+             <div>
+             <h4>MySQL</h4>
+             <small className='text-light'>Beginner</small>
+             </div>
               </article>
 
-              <article className='details'>
-             <AiFillCheckCircle color="blue" size="25px"/>
-             <h4>React.Js</h4>
-             <small className='text-light'>Experienced</small>
-              </article>
 
-              <article className='details'>
-             <AiFillCheckCircle color="blue" size="25px"/>
-             <h4>Redux</h4>
-             <small className='text-light'>Experienced</small>
-              </article>
 
-              <article className='details'>
-             <AiFillCheckCircle color="blue" size="25px"/>
-             <h4>Boostrap</h4>
-             <small className='text-light'>Experienced</small>
-              </article>
-
-              <article className='details'>
-             <AiFillCheckCircle color="blue" size="25px"/>
-             <h4>Tailwind</h4>
-             <small className='text-light'>Experienced</small>
-              </article>
-
+    </div>
+            
 
           </div>
       </div>
