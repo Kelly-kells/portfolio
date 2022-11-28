6dfb80f9769236dace3d92731f7a44bc822520f3
Kelly-kells <kellskelechi@gmail.com>
2022-11-28 02:10:43 +0100

    services

--- src/Components/services/Services.jsx
+++ src/Components/services/Services.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import "./services.css"
+import { BiCheck } from "react-icons/bi"
 const Services = () => {
   return (
     <section id='services'>
@@ -8,8 +9,37 @@ const Services = () => {
       <div className="container services__container">
         <article className='service'>
           <div className="service-head">
-
+            <h3>UI Design</h3>
           </div>
+          <ul className='service-list'>
+            <li>
+              <BiCheck />
+              <p>Lorem ipsum dolor sit amet consectetur.</p>
+            </li>
+
+            <li>
+              <BiCheck />
+              <p>Lorem ipsum dolor sit amet consectetur.</p>
+            </li>
+
+            <li>
+              <BiCheck />
+              <p>Lorem ipsum dolor sit amet consectetur.</p>
+            </li>
+
+            <li>
+              <BiCheck />
+              <p>Lorem ipsum dolor sit amet consectetur.</p>
+            </li>
+            <li>
+              <BiCheck />
+              <p>Lorem ipsum dolor sit amet consectetur.</p>
+            </li>
+            <li>
+              <BiCheck />
+              <p>Lorem ipsum dolor sit amet consectetur.</p>
+            </li>
+          </ul>
 
         </article>
       </div>
