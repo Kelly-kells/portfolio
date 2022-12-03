ad721d1fd79afa6f9692681d64a48f7739f12cba
Kelly-kells <kellskelechi@gmail.com>
2022-12-03 18:21:19 +0100

    project

--- src/Components/portfolio/Portfolio.jsx
+++ src/Components/portfolio/Portfolio.jsx
@@ -2,7 +2,20 @@ import React from 'react'
 
 const Portfolio = () => {
   return (
-    <section id='portfolio'>Portfolio</section>
+    <section id='portfolio'>
+
+      <h5>my resent work</h5>
+      <h2>portfolio</h2>
+
+      <div className="container portfolio__container">
+        <article className="portfolio__item">
+          <div className="portfolio__item-image">
+          </div>
+          <h3>Project_ai</h3>
+          <a href=""></a>git
+        </article>
+      </div>
+    </section>
   )
 }
 
