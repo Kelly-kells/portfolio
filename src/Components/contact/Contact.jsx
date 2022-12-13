2facd7dbe37c85f2d75d35b8305fadf28f1f026b
Kelly-kells <kellskelechi@gmail.com>
2022-12-14 00:37:59 +0100

    form

--- src/Components/contact/Contact.jsx
+++ src/Components/contact/Contact.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
-import {MdEmail} from "react-icons/md"
-
+import { MdEmail } from "react-icons/md"
+import { BsTwitter } from "react-icons/bs"
+import { RiWhatsappFill } from "react-icons/ri"
 const Contact = () => {
   return (
     <section id='contact'>
@@ -10,29 +11,31 @@ const Contact = () => {
       <h2>contact mes</h2>
       <div className="container contact__container">
         <div className="contact__options">
-    <article className='contacts'>
-      <MdEmail/>
-    <h4>Email</h4>
-    <h5>kellskelechi@gmail.com</h5>
-    <a href="mailto: kellskelechi@gmail.com">Send a message</a>
-    </article>
+          <article className='contacts'>
+            <MdEmail />
+            <h4>Email</h4>
+            <h5>kellskelechi@gmail.com</h5>
+            <a href="mailto:kellskelechi@gmail.com" target="_blank">Send a message</a>
+          </article>
 
-    <article className='contacts'>
-      <MdEmail/>
-    <h4>Email</h4>
-    <h5>kellskelechi@gmail.com</h5>
-    <a href="mailto: kellskelechi@gmail.com">Send a message</a>
-    </article>
+          <article className='contacts'>
+            <BsTwitter />
+            <h4>Twitter</h4>
+            <h5>KelechiKells</h5>
+            <a href="https://twitter.com/KelechiKells" target="_blank">Send a message</a>
+          </article>
 
-    <article className='contacts'>
-      <MdEmail/>
-    <h4>Email</h4>
-    <h5>kellskelechi@gmail.com</h5>
-    <a href="mailto: kellskelechi@gmail.com">Send a message</a>
-    </article>
+          <article className='contacts'>
+            <RiWhatsappFill />
+            <h4>Whatsapp</h4>
+            <h5>kellskelechi@gmail.com</h5>
+            <a href="https://api.whatsapp.com/send?phone=234702727777" target="_blank">Send a message</a>
+          </article>
         </div>
 
-        <form action=""></form>
+        <form action="">
+
+        </form>
 
       </div>
     </section>
